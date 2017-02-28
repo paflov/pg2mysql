cc743c312e92e9b002ad84fe093e122dc7fb51fe
Philipp Franck <philipp.franck@dainst.de>
2017-02-28 11:51:40 +0100

    frist corrections

--- pg2mysql.php
+++ pg2mysql.php
@@ -30,23 +30,25 @@ include "pg2mysql.inc.php";
 
 The <b>pg2mysql</b> converter is an online tool to convert/migrate existing PostgreSQL databases into MySQL.  Simply dumping from Postgres and importing to MySQL does not work because of differences in syntax and data types.  To use this converter, just create a postgres SQL dump (<tt>pg_dump -U username -s dbname &gt; dbname.sql</tt>), and copy and paste it into the text area below.  Click the <b>Convert to MySQL</b> button and the page will re-load with your new MySQL code that you can copy out of the textarea into a text editor to save and import into your MySQL database.
 <br><br>
-<form method=post action="pg2mysql.php">
+<i>Since lightbox does not support pg2mysql anymore, find this slightly actualized version here: <a href="https://github.com/paflov/pg2mysql">https://github.com/paflov/pg2mysql</a></i>
+<br><br>
+<form method="post" action="pg2mysql.php">
 
-<?
- if($_POST['postgresdata'])
+<?php
+ if(isset($_POST['postgresdata']))
  {
  	$output=pg2mysql(stripslashes($_POST['postgresdata']));
  	echo "<h3>Here is your MySQL dump file</h3>";
-	echo "<textarea rows=20 cols=80 name=mysqldata>$output</textarea>";
+	echo "<textarea rows='20' cols='80' name='mysqldata'>$output</textarea>";
  }
  else
  {
  ?>
  	<h3>Paste PostgreSQL dump file here</h3>
-	<textarea rows=20 cols=80 name=postgresdata></textarea>
+	<textarea rows="20" cols="8"0 name="postgresdata"></textarea>
 	<br />
-	<input type=submit value="Convert to MySQL">
- <?
+	<input type="submit" value="Convert to MySQL">
+ <?php
  }
  ?>
 
